c5d4390af71d9cb346dbd6a1662d360cf5becbe3
fumiama <41315874+fumiama@users.noreply.github.com>
2022-08-14 13:59:22 +0800

    feat: 更易于使用的openjtalk和monotonic_align及代码精简

--- commons.py
+++ commons.py
@@ -26,34 +26,10 @@ def get_padding(kernel_size, dilation=1):
   return int((kernel_size*dilation - dilation)/2)
 
 
-def convert_pad_shape(pad_shape):
-  l = pad_shape[::-1]
-  pad_shape = [item for sublist in l for item in sublist]
-  return pad_shape
-
-
 def intersperse(lst, item):
   result = [item] * (len(lst) * 2 + 1)
   result[1::2] = lst
   return result
-
-
-def kl_divergence(m_p, logs_p, m_q, logs_q):
-  """KL(P||Q)"""
-  kl = (logs_q - logs_p) - 0.5
-  kl += 0.5 * (torch.exp(2. * logs_p) + ((m_p - m_q)**2)) * torch.exp(-2. * logs_q)
-  return kl
-
-
-def rand_gumbel(shape):
-  """Sample from the Gumbel distribution, protect from overflows."""
-  uniform_samples = torch.rand(shape) * 0.99998 + 0.00001
-  return -torch.log(-torch.log(uniform_samples))
-
-
-def rand_gumbel_like(x):
-  g = rand_gumbel(x.size()).to(dtype=x.dtype, device=x.device)
-  return g
 
 
 def slice_segments(x, ids_str, segment_size=4):
@@ -75,34 +51,6 @@ def rand_slice_segments(x, x_lengths=None, segment_size=4):
   return ret, ids_str
 
 
-def get_timing_signal_1d(
-    length, channels, min_timescale=1.0, max_timescale=1.0e4):
-  position = torch.arange(length, dtype=torch.float)
-  num_timescales = channels // 2
-  log_timescale_increment = (
-      math.log(float(max_timescale) / float(min_timescale)) /
-      (num_timescales - 1))
-  inv_timescales = min_timescale * torch.exp(
-      torch.arange(num_timescales, dtype=torch.float) * -log_timescale_increment)
-  scaled_time = position.unsqueeze(0) * inv_timescales.unsqueeze(1)
-  signal = torch.cat([torch.sin(scaled_time), torch.cos(scaled_time)], 0)
-  signal = F.pad(signal, [0, 0, 0, channels % 2])
-  signal = signal.view(1, channels, length)
-  return signal
-
-
-def add_timing_signal_1d(x, min_timescale=1.0, max_timescale=1.0e4):
-  b, channels, length = x.size()
-  signal = get_timing_signal_1d(length, channels, min_timescale, max_timescale)
-  return x + signal.to(dtype=x.dtype, device=x.device)
-
-
-def cat_timing_signal_1d(x, min_timescale=1.0, max_timescale=1.0e4, axis=1):
-  b, channels, length = x.size()
-  signal = get_timing_signal_1d(length, channels, min_timescale, max_timescale)
-  return torch.cat([x, signal.to(dtype=x.dtype, device=x.device)], axis)
-
-
 def subsequent_mask(length):
   mask = torch.tril(torch.ones(length, length)).unsqueeze(0).unsqueeze(0)
   return mask
@@ -122,11 +70,6 @@ def convert_pad_shape(pad_shape):
   l = pad_shape[::-1]
   pad_shape = [item for sublist in l for item in sublist]
   return pad_shape
-
-
-def shift_1d(x):
-  x = F.pad(x, convert_pad_shape([[0, 0], [0, 0], [1, 0]]))[:, :, :-1]
-  return x
 
 
 def sequence_mask(length, max_length=None):
@@ -152,21 +95,3 @@ def generate_path(duration, mask):
   path = path - F.pad(path, convert_pad_shape([[0, 0], [1, 0], [0, 0]]))[:, :-1]
   path = path.unsqueeze(1).transpose(2,3) * mask
   return path
-
-
-def clip_grad_value_(parameters, clip_value, norm_type=2):
-  if isinstance(parameters, torch.Tensor):
-    parameters = [parameters]
-  parameters = list(filter(lambda p: p.grad is not None, parameters))
-  norm_type = float(norm_type)
-  if clip_value is not None:
-    clip_value = float(clip_value)
-
-  total_norm = 0
-  for p in parameters:
-    param_norm = p.grad.data.norm(norm_type)
-    total_norm += param_norm.item() ** norm_type
-    if clip_value is not None:
-      p.grad.data.clamp_(min=-clip_value, max=clip_value)
-  total_norm = total_norm ** (1. / norm_type)
-  return total_norm
--- monotonic_align/__init__.py
+++ monotonic_align/__init__.py
@@ -1,16 +1,9 @@
 from numpy import zeros, int32, float32
 from torch import from_numpy
 
-from os import system
-system("cd monotonic_align && python setup.py build_ext --inplace")
-
-from .monotonic_align.core import maximum_path_c
+from .core import maximum_path_jit
 
 def maximum_path(neg_cent, mask):
-  """ Cython optimized version.
-  neg_cent: [b, t_t, t_s]
-  mask: [b, t_t, t_s]
-  """
   device = neg_cent.device
   dtype = neg_cent.dtype
   neg_cent = neg_cent.data.cpu().numpy().astype(float32)
@@ -18,5 +11,5 @@ def maximum_path(neg_cent, mask):
 
   t_t_max = mask.sum(1)[:, 0].data.cpu().numpy().astype(int32)
   t_s_max = mask.sum(2)[:, 0].data.cpu().numpy().astype(int32)
-  maximum_path_c(path, neg_cent, t_t_max, t_s_max)
+  maximum_path_jit(path, neg_cent, t_t_max, t_s_max)
   return from_numpy(path).to(device=device, dtype=dtype)
